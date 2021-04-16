7499bea7fa643cae45733f9c87b067577ae9af6e
mgtablante <mgtablantel@gmail.com>
2021-04-16 15:57:39 +0000

    first commit1

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,35 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import React, { useState } from "react";
+// import PropTypes from "prop-types";
 //create your first component
+
 export function Home() {
+	const [todo, setTodo] = useState(["Make the bed", "Wash my hands"]);
+	const [inputValue, setInputValue] = useState("");
+
+	const handleChange = () => {
+		setInputValue(event.target.value);
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1 className="text-center">TODOS</h1>
+			<from className="Formulario">
+				<input
+					value={inputValue}
+					type="text"
+					placeholder="What needs to be done?"
+					className="form-control my-2"
+					onChange={handleChange}></input>
+				<button className="btn btn-block btn-primary"> Agregar</button>
+			</from>
+			<div className="listas-tareas mt-3">
+				<div className="alert alert-warning d-flex">
+					<p>{todo}</p>
+					<h3 className="m-3">
+						<i className="fas fa-times"></i>
+					</h3>
+				</div>
+			</div>
 		</div>
 	);
 }
