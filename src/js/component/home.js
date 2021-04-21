4f0763ee8149f10441d899a5e6bdd8c2e1a73b60
mgtablante <mgtablantel@gmail.com>
2021-04-21 06:57:36 +0000

    2do sub

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -4,12 +4,16 @@ import React, { useState } from "react";
 
 export function Home() {
 	const [todo, setTodo] = useState(["Make the bed", "Wash my hands"]);
-	const [inputValue, setInputValue] = useState("");
+	const [inputValue, setInputValue] = useState([]);
 
 	const handleChange = () => {
 		setInputValue(event.target.value);
+		console.log("inputValue ", inputValue);
 	};
-
+	const handleKeyPress = event => {
+		//que reciba texto pero si esta vacio que no se ejecute, especificar tecla que acepte la orden.
+		//que setee el valor de todo con el valor que viene del input (inputValue)
+	};
 	return (
 		<div className="container">
 			<h1 className="text-center">TODOS</h1>
@@ -19,8 +23,9 @@ export function Home() {
 					type="text"
 					placeholder="What needs to be done?"
 					className="form-control my-2"
-					onChange={handleChange}></input>
-				<button className="btn btn-block btn-primary"> Agregar</button>
+					onChange={handleChange}
+					//onKeyPress=
+				></input>
 			</from>
 			<div className="listas-tareas mt-3">
 				<div className="alert alert-warning d-flex">
